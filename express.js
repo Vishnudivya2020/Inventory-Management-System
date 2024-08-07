98e49f31a53a006c0cb69058c846ee3582da1ecb
vishnudivya2020 <vishnudivyabharathi@gmail.com>
2024-08-07 10:23:35 +0300

    Added core

--- express.js
+++ express.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import mongooseConnect from './db-utils/mongoose-connection.js';
 import CustomerRouter from './routes/CustomerRouter.js';
 import ProductRouter from './routes/ProductsRouter.js';
@@ -9,6 +10,7 @@ await mongooseConnect();
 
 
 server.use(express.json());
+server.use(cors());
 
 
  server.use("/Products",ProductRouter);
